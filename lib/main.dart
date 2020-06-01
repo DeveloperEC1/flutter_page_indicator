3ee7f371fb4863884860c6dc271ae4fac905f4a8
EliorCohen <elior123456123123@gmail.com>
2020-06-02 00:47:24 +0300

    update

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutterpageindicator/page_indicator_example.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter PageIndicator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PageIndicatorExample(),
+    );
+  }
+}
---
+++ lib/page_indicator_example.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:page_indicator/page_indicator.dart';
+
+class PageIndicatorExample extends StatefulWidget {
+  PageIndicatorExample({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<PageIndicatorExample> {
+  PageController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: PageIndicatorContainer(
+        indicatorSelectorColor: Color(0xff2cbbba),
+        indicatorColor: Color(0xffa2e2e2),
+        child: PageView(
+          children: <Widget>[
+            Center(child: Text('Page one')),
+            Center(child: Text('Page two')),
+            Center(child: Text('Page three')),
+          ],
+          controller: controller,
+        ),
+        align: IndicatorAlign.bottom,
+        length: 3,
+        indicatorSpace: 13.0,
+      ),
+    );
+  }
+}
